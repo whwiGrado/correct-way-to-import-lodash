4c869b307dd88d2e4d0b46934f97e9eb662bfcdf
wanghongwei <wanghongwei@bytedance.com>
2018-12-02 10:05:23 +0800

    add source-map-explorer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,17 +6,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/whwiGrado/correct-way-to-import-lodash"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn the Correct Way to Import Lodash Libraries
           </a>
         </header>
       </div>
