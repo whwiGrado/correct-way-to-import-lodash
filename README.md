3f6d51068f1684a405be0fbf91ed49d99beb6f56
wanghongwei <wanghongwei@bytedance.com>
2018-12-02 11:00:31 +0800

    add lodash import methods analysis

--- README.md
+++ README.md
@@ -4,6 +4,90 @@ this repo is create by `create-react-app`
 
 # Installation
 ```bash
-$ yarn add source-map-explorer
+$ yarn install
 ```
 
+# Analyze the bundle size
+```bash
+$ npm run analyze
+```
+
+# Four Ways to Import Lodash
+
+### 1. Import the whole library:
+
+```bash
+$ yarn add lodash
+```
+
+```jsx harmony
+import _ from 'lodash';
+```
+
+- Pros:
+  - Only one import line
+ 
+- Cons:
+  - It seems like the import of a whole library will lead to the largest bundle size
+  - Less readable usage in the javascript code
+ 
+
+### 2. Import specific methods inside of curly brackets:
+ 
+```bash
+$ yarn add lodash
+```
+
+```jsx harmony
+import { get, tail, times, uniq } from 'lodash';
+```
+
+- Pros:
+  - Only one import line (for a decent amount of functions)
+  - More readable usage: get() instead of _.get() later in the javascript code
+
+- Cons:
+  - Every time we want use a new function or stop using another - it needs to be maintained and managed
+ 
+
+### 3. Import specific methods one by one:
+
+```bash
+$ yarn add lodash
+```
+
+```jsx harmony
+import get from 'lodash/get';
+import tail from 'lodash/tail';
+import times from 'lodash/times';
+import uniq from 'lodash/uniq';
+```
+
+- Pros:
+  - Seems to be the smallest bundle size.
+  - More readable usage: get() instead of _.get()
+
+- Cons:
+  - The import maintenance is much more complicated than the previous options
+  - Lots of import lines in the head of the file don’t look nice and readable.
+
+### 4. Import specific methods and Install one by one:
+
+```bash
+$ yarn add lodash.get lodash.tail lodash.times lodash.uniq
+```
+
+```jsx harmony
+import get from 'lodash.get';
+import tail from 'lodash.tail';
+import times from 'lodash.times';
+import uniq from 'lodash.uniq';
+```
+
+- Pros:
+  - Seems to be the smallest bundle size.
+  - More readable usage: get() instead of _.get()
+
+- Cons:
+  - The import maintenance is much more complicated than the previous options
+  - Lots of import lines in the head of the file don’t look nice and readable.
--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Import the whole library
+// import _ from 'lodash';
+
+// Import specific methods inside of curly brackets
+// import { get, tail, times, uniq } from 'lodash';
+
+// Import specific methods inside of curly brackets
+// import get from 'lodash/get';
+// import tail from 'lodash/tail';
+// import times from 'lodash/times';
+// import uniq from 'lodash/uniq';
+
+// Import specific methods inside of curly brackets
+import get from 'lodash.get';
+import tail from 'lodash.tail';
+import times from 'lodash.times';
+import uniq from 'lodash.uniq';
+
+const FIELD_TYPE_CONFIG = {
+  number: {
+    styles: {
+      textAlign: 'right',
+    },
+  },
+};
+
 class App extends Component {
+  getFieldTypeConfig(path) {
+    if (typeof path === 'string' || Array.isArray(path)) {
+      // return _.get(FIELD_TYPE_CONFIG, path)
+      return get(FIELD_TYPE_CONFIG, path)
+    }
+    return {};
+  };
+
   render() {
+
+    const numbers = [1, 5, 8, 10, 1, 5, 15, 42, 5];
+
+    // const uniqNumbers = _.uniq(numbers);
+    const uniqNumbers = uniq(numbers);
+    // Result: [1, 5, 8, 10, 15, 42]
+
+    // const tailNumbers = _.tail(numbers);
+    const tailNumbers = tail(numbers);
+    // Result: [5, 8, 10, 1, 5, 15, 42, 5]
+
+    const getRandomNumber = () => { return Math.round(Math.random() * 100); };
+
+    // const randomNumbers = _.times(8, getRandomNumber);
+    const randomNumbers = times(8, getRandomNumber);
+    // Result: [58, 9, 98, 54, 96, 24, 25, 74]
+
+    const styles = this.getFieldTypeConfig('number.styles');
     return (
       <div className="App">
         <header className="App-header">
@@ -14,6 +66,18 @@ class App extends Component {
           >
             Learn the Correct Way to Import Lodash Libraries
           </a>
+          <ul>
+            <li>base numbers array: {JSON.stringify(numbers)}</li>
+            <li>uniqNumbers: {JSON.stringify(uniqNumbers)}</li>
+            <li>tailNumbers: {JSON.stringify(tailNumbers)}</li>
+            <li>randomNumbers: {JSON.stringify(randomNumbers)}</li>
+          </ul>
+          <ul>
+            <li>base object: </li>
+            <li>{JSON.stringify(FIELD_TYPE_CONFIG, 0, 2)}</li>
+            <li>get number.styles from object: </li>
+            <li>{JSON.stringify(styles, 0, 2)}</li>
+          </ul>
         </header>
       </div>
     );
